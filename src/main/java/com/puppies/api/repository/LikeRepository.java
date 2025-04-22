a9d817aa9ea2f41b9eed22a833c1b734e17020fe
Andres Calderon <andresfcaalderon@hotmail.com>
2025-04-22 12:13:35 -0500

    Create Repositories for entities

---
+++ src/main/java/com/puppies/api/repository/LikeRepository.java
@@ -0,0 +1,14 @@
+package com.puppies.api.repository;
+
+import com.puppies.api.model.Like;
+import com.puppies.api.model.Post;
+import com.puppies.api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface LikeRepository extends JpaRepository<Like, Long> {
+    Optional<Like> findByUserAndPost(User user, Post post);
+    // You can add custom query methods here
+}
